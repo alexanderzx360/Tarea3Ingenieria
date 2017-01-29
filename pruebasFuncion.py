3e47ef174f3b9345ee666a4c02e15d2995373471
alexander <alexander@alexander-VirtualBox>
2017-01-29 14:57:15 -0400

    Inicio de Pruebas Funcion

---
+++ pruebasFuncion.py
@@ -0,0 +1,22 @@
+'''
+Created on Jan 29, 2017
+
+@author: cinfante
+@author: aromero
+'''
+import unittest
+
+class Test(unittest.TestCase):
+    tarifaPrueba = None
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
