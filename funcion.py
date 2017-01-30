af447890edda5e28ea22a21d4d3e795a28b73755
alexander <alexander@alexander-VirtualBox>
2017-01-29 20:15:03 -0400

    Atributos 1) 2)

--- funcion.py
+++ funcion.py
@@ -6,7 +6,22 @@ Created on Jan 29, 2017
 '''
 
 class BilleteraElectronica:
-    identificador = 0
-    nombres = ""
-    apellidos = ""
-    PIN = 0
+    _identificador = 0
+    _nombres = ""
+    _apellidos = ""
+    _CI = 0
+    _PIN = 0
+    
+    def __init__(self, identificador, nombres, apellidos, CI, PIN):
+        if PIN < 0 or pin > 10000:
+            raise Exception('El PIN debe ser de 4 numeros MMG')
+        if CI <= 0:
+            raise Exception("Debe introducir una cédula válida")
+        self._identificador = identificador
+        self._nombres = nombres
+        self._apellidos = apellidos
+        self._CI = CI
+        self._PIN = PIN
+    
+    def getPIN(self):
+        return self._PIN
